6b6d84528b3006280f6a992be35d3285b73a0bf5
harrycaveman <hjoseph@unca.edu>
2018-05-09 00:54:34 -0400

    removed handlebars from old copypasta

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const exphbs = require("express-handlebars");
 const PORT = process.env.PORT || 3001;
 // Initialize Express
 var app = express();
