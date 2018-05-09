bb65c1d018f70e7a1ba39f62e302bae1d496386b
harrycaveman <hjoseph@unca.edu>
2018-05-09 03:23:54 -0400

    found missing link to statics in server

--- controllers/routes/index.js
+++ controllers/routes/index.js
@@ -10,7 +10,7 @@ const router = require('express').Router();
 	    res.sendFile(
 			path.join(
 				__dirname, 
-				'../../client/build/index.html'
+				'../../client/public/index.html'
 			)
 		)
 	);
